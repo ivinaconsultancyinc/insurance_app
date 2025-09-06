4aab2b978c31a535fba2c0499230c25fdb54603c
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-09-06 06:53:17 +0000

    Update routers_customer.py

--- routers/routers_customer.py
+++ routers/routers_customer.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 
-from insurance_app import models, schemas
-from insurance_app.database import get_db
-from insurance_app.services import customer_service
+from models import models, schemas
+from database import get_db
+from services import customer_service
 
 router = APIRouter(
     prefix="/customers",
@@ -30,4 +30,5 @@ def update_customer(customer_id: int, customer: schemas.CustomerUpdate, db: Sess
 @router.delete("/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_customer(customer_id: int, db: Session = Depends(get_db)):
     customer_service.delete_customer(db, customer_id)
-    return 
+    return None
+
