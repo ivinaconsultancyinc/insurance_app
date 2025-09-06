2e11af609e7d3203b53016ca9c2b8a558ae1d05a
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-09-06 05:53:54 +0000

    Update client.py

--- models/client.py
+++ models/client.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Enum, JSON
 from sqlalchemy.dialects.postgresql import UUID
-from insurance_app.database import Base
+from database import Base
 import uuid
 
 class Client(Base):
@@ -11,4 +11,5 @@ class Client(Base):
     client_type = Column(Enum("Individual", "Corporate", name="client_type_enum"), nullable=False)
     contact_info = Column(JSON, nullable=True)
     kyc_documents = Column(JSON, nullable=True)
+
     status = Column(Enum("Active", "Inactive", "Blocked", name="client_status_enum"), default="Active")
