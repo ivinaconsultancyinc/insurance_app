83b1fc1a7f56b983a62e48590f20c5875ad313d9
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-09-06 06:35:54 +0000

    Update routers_agent.py

--- routers/routers_agent.py
+++ routers/routers_agent.py
@@ -2,32 +2,33 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 
-from insurance_app import models, schemas
-from insurance_app.database import get_db
-from insurance_app.services import agent_service
+from schemas import agent as agent_schema
+from services import agent as agent_service
+from database import get_db
 
 router = APIRouter(
     prefix="/agents",
     tags=["Agents"]
 )
 
-@router.post("/", response_model=schemas.AgentOut, status_code=status.HTTP_201_CREATED)
-def create_agent(agent: schemas.AgentCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=agent_schema.AgentOut, status_code=status.HTTP_201_CREATED)
+def create_agent(agent: agent_schema.AgentCreate, db: Session = Depends(get_db)):
     return agent_service.create_agent(db, agent)
 
-@router.get("/", response_model=List[schemas.AgentOut])
+@router.get("/", response_model=List[agent_schema.AgentOut])
 def get_all_agents(db: Session = Depends(get_db)):
     return agent_service.get_all_agents(db)
 
-@router.get("/{agent_id}", response_model=schemas.AgentOut)
+@router.get("/{agent_id}", response_model=agent_schema.AgentOut)
 def get_agent(agent_id: int, db: Session = Depends(get_db)):
     return agent_service.get_agent_by_id(db, agent_id)
 
-@router.put("/{agent_id}", response_model=schemas.AgentOut)
-def update_agent(agent_id: int, agent: schemas.AgentUpdate, db: Session = Depends(get_db)):
+@router.put("/{agent_id}", response_model=agent_schema.AgentOut)
+def update_agent(agent_id: int, agent: agent_schema.AgentUpdate, db: Session = Depends(get_db)):
     return agent_service.update_agent(db, agent_id, agent)
 
 @router.delete("/{agent_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_agent(agent_id: int, db: Session = Depends(get_db)):
     agent_service.delete_agent(db, agent_id)
     return None
+
