cc061c85820d76ccbfd5d10b5e26d383c06d21d4
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-09-08 05:09:53 +0000

    Update routers_customer.py

--- routers/routers_customer.py
+++ routers/routers_customer.py
@@ -1,36 +1,31 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
-
-import models, schemas
+import models
+from schemas.customer_schema import CustomerCreate, CustomerUpdate, CustomerOut
 from database import get_db
 from services import customer_service
-
 router = APIRouter(
     prefix="/customers",
     tags=["Customers"]
 )
-
-@router.post("/", response_model=schemas.CustomerOut, status_code=status.HTTP_201_CREATED)
-def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=CustomerOut, status_code=status.HTTP_201_CREATED)
+def create_customer(customer: CustomerCreate, db: Session = Depends(get_db)):
     return customer_service.create_customer(db, customer)
-
-@router.get("/", response_model=List[schemas.CustomerOut])
+@router.get("/", response_model=List[CustomerOut])
 def get_all_customers(db: Session = Depends(get_db)):
     return customer_service.get_all_customers(db)
-
-@router.get("/{customer_id}", response_model=schemas.CustomerOut)
+@router.get("/{customer_id}", response_model=CustomerOut)
 def get_customer(customer_id: int, db: Session = Depends(get_db)):
     return customer_service.get_customer_by_id(db, customer_id)
-
-@router.put("/{customer_id}", response_model=schemas.CustomerOut)
-def update_customer(customer_id: int, customer: schemas.CustomerUpdate, db: Session = Depends(get_db)):
+@router.put("/{customer_id}", response_model=CustomerOut)
+def update_customer(customer_id: int, customer: CustomerUpdate, db: Session = Depends(get_db)):
     return customer_service.update_customer(db, customer_id, customer)
-
 @router.delete("/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_customer(customer_id: int, db: Session = Depends(get_db)):
     customer_service.delete_customer(db, customer_id)
     return None
+
 
 
 
